07c1c2799f286fab6a29244ec414204cde5580a8
L-Hejer <laouani.h@gmail.com>
2021-01-20 11:26:02 +0100

    Call Comp1 in App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from "./logo.svg";
-import "./App.css";
-import { Comp3 } from "./Components/Comp3";
+import logo from './logo.svg';
+import './App.css';
+import { Comp3 } from './Components/Comp3';
+import Comp2 from './Components/Comp2';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Comp2 />
       <Comp3 />
     </div>
   );
