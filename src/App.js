6349f1d0c2ec68c8b005201750048d3be9ff6f90
mohamedhan <mohamedhanafihassanet@gmail.com>
2021-01-20 11:20:48 +0100

    comp 3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Comp3 } from "./Components/Comp3";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Comp3 />
     </div>
   );
 }
